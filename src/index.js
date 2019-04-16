67a1d85122bd1035aaff461e58035b4d792a7d22
iliatcymbal <iliatcymbal>
2019-04-16 19:15:33 +0300

    Remove almost all

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const foo = (...args) => args.join(' - ');
-
-document.body.innerHTML += foo(1,2,3)
-
-if (module.hot) {
-    module.hot.accept();
-}
--- src/tests/tools.js
+++
@@ -1,3 +0,0 @@
-export const sum = (a = 0, b = 0) => a + b;
-
-export const foo = (a = 0, b = 0) => {};
